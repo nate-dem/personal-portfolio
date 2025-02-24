1b2a09eeaa60dfd16d13084f8ca73b356be22765
nate-dem <natedem@stanford.edu>
2025-02-23 18:03:03 -0800

    Added about me page and wrote initial section

--- src/App.jsx
+++ src/App.jsx
@@ -68,13 +68,22 @@ const Portfolio = () => {
         <div className='grid grid-rows-[20fr,80fr] grid-cols-[60fr,40fr] h-full'>
           <div></div>
           <div></div>
-          <div className='flex items-start justify-center'>
-            <div className='flex flex-col items-start justify-center space-y-10'>
-              <h1 className='text-white text-3xl font-mono'>
+          <div className='flex items-start justify-center px-12'>
+            <div className='flex flex-col items-start justify-center space-y-10 text-gray-300'>
+              <h1 className='text-3xl font-mono'>
                 About Me:
               </h1>
-              <p className='text-gray-300 text-xl'>
-
+              <p className='text-xl'>
+                After graduating high school in 2021, I began my journey at CUNY Macaulay Honors College at Baruch, focusing on biology and computer science. During my time there, I was fortunate to explore diverse interests and experiences:
+              </p>
+              <ul className="list-disc list-inside space-y-2">
+                <li className="text-lg">Served as an EMT responding to emergency calls across Long Island, Manhattan, and the Bronx.</li>
+                <li className="text-lg">Balanced academics with athletics as captain of the varsity baseball team.</li>
+                <li className='text-lg'>Led our team to win the 2022 CUNYAC Championship and earned 2023 CUNYAC Pitcher of the Year.</li>
+                <li className="text-lg">Conducted research on gene expression patterns in complex anatomical structures in mammals.</li>
+              </ul>
+              <p className='text-xl'>
+                While initially pursing the pre-med track with hopes of attending medical school, my plans changed after becoming fascinated with the intersection of technology and healthcare. I transferred to Stanford University in 2023 to study computer science and computational biology. This past summer, I interned at an AI startup where I researched bias assessment in large language models. Currently, I'm working on a collaborative project between the Stanford School of Medicine and the Palo Alto VA Medical Center, integrating healthcare wearables with augmented-reality tools to improve psychiatric assessments. This summer, I will be interning at Amazon as a Software Development Engineer.
               </p>
             </div>      
           </div>
